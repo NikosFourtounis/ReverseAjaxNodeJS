fdc279618ddbffd3d93cc03e1f915b510ea5f0bc
N Fourtounis <nickfortune@windowslive.com>
2018-02-13 11:56:38 +0200

    server.html test

--- service.js
+++ service.js
@@ -1,13 +1,19 @@
 var Pusher = require('pusher');
-
 var pusher = new Pusher({
-  appId: '474309',
-  key: 'af894f403c77e822ac1b',
-  secret: '3903517ae558839f84a9',
-  cluster: 'eu',
-  encrypted: true
+	appId: '474309',
+	key: 'af894f403c77e822ac1b',
+	secret: '3903517ae558839f84a9',
+	cluster: 'eu',
+	encrypted: true
 });
-
+var d = new Date();
 pusher.trigger('my-channel', 'my-event', {
-  "message": "hello world"
-});
+	"message": d
+});/*while (true) {
+	setTimeout(function(){
+		d = new Date();
+		pusher.trigger('my-channel', 'my-event', {
+			"message": d
+		});
+	}, 2000);
+}*/
